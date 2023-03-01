8073b09a115cf1eeb0e067c536d94266d4a6a87e
mamamamamaa <maks.denishchuk@gmail.com>
2023-03-01 15:28:30 +0200

    rm tailwind styles

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,7 +2,7 @@ import { Card } from "../Card/Card";
 
 export const App = () => {
   return (
-    <div className="container">
+    <div className="container py-4 px-2">
       <Card />
     </div>
   );
